9b89c285878da7fb802945c27bb15922bd74fba8
aishwarya <swamiaishwarya24@gmail.com>
2024-08-17 23:28:35 +0530

    launch browser

--- Udemy/launch_browser.py
+++ Udemy/launch_browser.py
@@ -1,9 +1,20 @@
+# from playwright.sync_api import sync_playwright
+# with sync_playwright() as p:
+#     browser=p.chromium.launch(headless=False,slow_mo=500)
+#     page=browser.new_page()
+#     page.goto('https://playwright.dev/python/')
+#     page.get_by_role('link',name='Docs').click()
+#     print(page.url)
+#     page.wait_for_timeout(5000)
+#     browser.close()
+
+
 from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser=p.chromium.launch(headless=False,slow_mo=500)
-    page=browser.new_page()
+    context=browser.new_context()
+    page=context.new_page()
     page.goto('https://playwright.dev/python/')
-    page.get_by_role('link',name='Docs').click()
-    print(page.url)
+    print("playwright url launched successfully")
     page.wait_for_timeout(5000)
     browser.close()
