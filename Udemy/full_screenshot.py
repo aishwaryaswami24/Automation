79368d50afa7562d4a5c4972f6c8cdbf98d549cb
aishwarya <swamiaishwarya24@gmail.com>
2024-08-27 23:22:29 +0530

    page.set_viewport_size

--- Udemy/full_screenshot.py
+++ Udemy/full_screenshot.py
@@ -5,5 +5,6 @@ with sync_playwright() as p:
     page=context.new_page()
     page.goto('https://playwright.dev/python/')
     page.wait_for_timeout(2000)
-    page.screenshot(path='playwright.png')
+    page.set_viewport_size({'height': 200,'width': 300})
+    page.screenshot(path='playwright.png',full_page=True)
     browser.close()
