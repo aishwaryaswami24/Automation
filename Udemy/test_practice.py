55fd433d16bfafa691f8a19ff126ffc359f312a9
aishwarya <swamiaishwarya24@gmail.com>
2024-08-29 23:17:40 +0530

    Overlapping Test Function Name:

--- Udemy/test_practice.py
+++ Udemy/test_practice.py
@@ -1,4 +1,5 @@
 import pytest
+import sys
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope='module')
@@ -17,13 +18,16 @@ def page(browser):
 
 
 
+@pytest.mark.skipif(sys.platform=='win32',reason='skip if platform version is win32')
 def test_site(page):
     page.goto('https://www.amazon.in/')
     page.wait_for_timeout(2000)
     assert page.title() == 'Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in'
+    print(page.title())
+
 
 @pytest.mark.skip(reason='test is not ready yet')
-def test_site(page):
+def test_site1(page):
     page.goto('https://www.instagram.com/')
     page.wait_for_timeout(2000)
     assert page.url == 'https://www.instagram.com/'
